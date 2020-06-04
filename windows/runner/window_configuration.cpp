bc1af668d904384b4b4636593009f3a02dd82269
Radical <romitradical@protonmail.com>
2020-06-04 07:35:57 +0530

    initial commit

---
+++ windows/runner/window_configuration.cpp
@@ -0,0 +1,7 @@
+#include "window_configuration.h"
+
+const wchar_t* kFlutterWindowTitle = L"flutter_designs_1_bank_1";
+const unsigned int kFlutterWindowOriginX = 10;
+const unsigned int kFlutterWindowOriginY = 10;
+const unsigned int kFlutterWindowWidth = 1280;
+const unsigned int kFlutterWindowHeight = 720;
